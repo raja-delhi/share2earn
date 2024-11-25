bcb255323d7354d533aed08ad7e2d2d2d84517d4
raja-delhi <nonofficial1234098@gmail.com>
2024-11-25 21:18:22 +0530

    Add files via upload

---
+++ src/main/java/com/share2earn/Share2earnApplication.java
@@ -0,0 +1,13 @@
+package com.share2earn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Share2earnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Share2earnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/share2earn/Share2earnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.share2earn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Share2earnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
